294804b783effc81ae0439c12d2eb796ad72286f
bharath <bharath131926@gmail.com>
2024-07-30 11:05:32 +0530

    this is git practice

---
+++ src/main/java/Selenium_Framework/GitPractice/GitDemo.java
@@ -0,0 +1,22 @@
+package Selenium_Framework.GitPractice;
+
+import org.testng.annotations.Test;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class GitDemo {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+		System.out.println("postJira1");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+
+}
